8294c387dd09fb769c76925a10ec31f588fbcfd9
Brendan Boerner <bboerner.foss@gmail.com>
2014-06-07 00:51:50 -0500

    a. Tweaks to linuxbrew to use coreutils commands w/o 'g' prefix. b. For gls aliases, use --color=auto. c. system/color.bash - consistently use '' in aliases.

--- system/color.bash
+++ system/color.bash
@@ -1,17 +1,17 @@
 # gls / grc / dircolors overides for ls
 if $(gls &>/dev/null) ; then
-    alias ls="gls -F --color"
-    alias gl="gls -lAh --color"
-    alias ll="gls -l --color"
-    alias la='gls -A --color'
-    alias dir='gls -lF --color'
-    alias vdir='gls -lF --color'
+    alias ls='gls -F --color=auto'
+    alias gl='gls -lAh --color=auto'
+    alias ll='gls -l --color=auto'
+    alias la='gls -A --color=auto'
+    alias dir='gls -lF --color=auto'
+    alias vdir='gls -lF --color=auto'
 
 elif [ -x /usr/bin/grc ]; then
-    alias ls="grc --colour=auto ls -F"
-    alias gl="grc --colour=auto ls -lAh"
-    alias ll="grc --colour=auto ls -l"
-    alias la="grc --colour=auto ls -A"
+    alias ls='grc --colour=auto ls -F'
+    alias gl='grc --colour=auto ls -lAh'
+    alias ll='grc --colour=auto ls -l'
+    alias la='grc --colour=auto ls -A'
     alias dir='grc --colour=auto ls -lF'
     alias vdir='grc --colour=auto vdir -lF'
 
@@ -19,9 +19,9 @@ elif [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
 
     alias ls='ls --color=auto'
-    alias gl="ls -lAh --colour=auto"
-    alias ll="ls -l --colour=auto"
-    alias la="ls -A --colour=auto"
+    alias gl='ls -lAh --colour=auto'
+    alias ll='ls -l --colour=auto'
+    alias la='ls -A --colour=auto'
     alias dir='dir -lF --color=auto'
     alias vdir='vdir --color=auto'
 
