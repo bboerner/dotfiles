2df7353a32a30c79d20c5c6fd09102f561a572a6
Brendan Boerner <bboerner.foss@gmail.com>
2014-08-15 02:07:19 -0500

    fix test for hub existence

--- hub/init.bash
+++ hub/init.bash
@@ -1,7 +1,7 @@
 # Git + Hub = GitHub
 # https://github.com/bboerner/hub
 
-hub 2>&1 > /dev/null
+(hub 2>&1) > /dev/null
 if [[ $? -eq 0 ]] ; then
     #echo "We have hub"
     eval "$(hub alias -s)"
