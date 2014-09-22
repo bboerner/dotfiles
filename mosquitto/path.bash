88b6b9eab12116dbcdd02895e82f43c8197765f6
Brendan Boerner <bboerner.foss@gmail.com>
2014-09-21 23:55:43 -0500

    . mosquitto/     . Rename mosquito to mosquitto     . Add sbin to $PATH     . Setup LD_LIBRARY_PATH

--- mosquitto/path.bash
+++ mosquitto/path.bash
@@ -1,5 +1,6 @@
 MOSQUITTO_ROOT="$HOME/usr/local/mosquitto"
 if [[ -d $MOSQUITTO_ROOT/bin ]]; then
-    export PATH="$MOSQUITTO_ROOT/bin:$PATH"
+    export PATH="$MOSQUITTO_ROOT/bin:$MOSQUITTO_ROOT/sbin:$PATH"
     [[ -d $MOSQUITTO_ROOT/man ]] && export MANPATH="$MOSQUITTO_ROOT/man:$MANPATH"
+    [[ -d $MOSQUITTO_ROOT/lib ]] && export LD_LIBRARY_PATH="$MOSQUITTO_ROOT/lib:$LD_LIBRARY_PATH"
 fi
