569cc465413372a2be36fe75e1d3c7548e058b27
Brendan Boerner <bboerner.foss@gmail.com>
2014-09-22 01:40:20 -0500

    Revert previous changes to zath/path.bash

--- zato/path.bash
+++ zato/path.bash
@@ -1,5 +1,13 @@
-ZATO_ROOT="/opt/zato/1.1"
-if [[ -d $ZATO_ROOT/bin ]]; then
-    export PATH="$ZATO_ROOT/bin:$PATH"
-    [[ -d $ZATO_ROOT/man ]] && export MANPATH="$ZATO_ROOT/man:$MANPATH"
-fi
+function add_zato() {
+    echo "Adding zato"
+
+    ZATO_ROOT="/opt/zato/1.1"
+    if [[ -d $ZATO_ROOT/bin ]]; then
+        export PATH="$ZATO_ROOT/bin:$PATH"
+        [[ -d $ZATO_ROOT/man ]] && export MANPATH="$ZATO_ROOT/man:$MANPATH"
+
+    else
+        echo "error: $ZATO_ROOT not found."
+
+    fi
+}
