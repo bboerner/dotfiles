c9743b36b7ae86dc14e7b1e3049a7325d0c532f7
Brendan Boerner <bboerner.foss@gmail.com>
2014-10-06 03:56:45 -0500

    Remove mosquito/ in favor of mosquitto/

--- mosquito/path.bash
+++
@@ -1,5 +0,0 @@
-MOSQUITTO_ROOT="$HOME/usr/local/mosquitto"
-if [[ -d $MOSQUITTO_ROOT/bin ]]; then
-    export PATH="$MOSQUITTO_ROOT/bin:$PATH"
-    [[ -d $MOSQUITTO_ROOT/man ]] && export MANPATH="$MOSQUITTO_ROOT/man:$MANPATH"
-fi
