d71f666da130179fb4d67cb6dfbcd9ad5ffb235d
Brendan Boerner <bboerner.foss@gmail.com>
2014-10-06 03:39:45 -0500

    . Install using full back to brew since no reason to believe it'll be in our PATH.
    . Add $BREW_ROOT/bin *only* to PATH. Don't add $BREW_LIB/lib to
    LD_LIBRARY_PATH.
    
    . Disable $BREW_OPT_LIBEXEC/gnubin and $BREW_OPT_LIBEXEC/gnuman.

--- linuxbrew/path.bash
+++ linuxbrew/path.bash
@@ -2,9 +2,10 @@ BREW_ROOT="$HOME/.linuxbrew"
 BREW_OPT_LIBEXEC="$BREW_ROOT/Cellar/coreutils/8.22/libexec"
 
 # Use the 'g' prefixed commands
-#[[ -d $BREW_ROOT/bin ]] && export PATH="$BREW_ROOT/bin:$PATH"
+[[ -d $BREW_ROOT/bin ]] && export PATH="$BREW_ROOT/bin:$PATH"
 #[[ -d $BREW_ROOT/lib ]] && export LD_LIBRARY_PATH="$BREW_ROOT/lib:$LD_LIBRARY_PATH"
 
 # w/o the 'g' prefix
-[[ -d $BREW_OPT_LIBEXEC/gnubin ]] && export PATH="$BREW_OPT_LIBEXEC/gnubin:$PATH"
-[[ -d $BREW_OPT_LIBEXEC/gnuman ]] && export MANPATH="$BREW_OPT_LIBEXEC/gnuman:$MANPATH"
+#[[ -d $BREW_OPT_LIBEXEC/gnubin ]] && export PATH="$BREW_OPT_LIBEXEC/gnubin:$PATH"
+#[[ -d $BREW_OPT_LIBEXEC/gnuman ]] && export MANPATH="$BREW_OPT_LIBEXEC/gnuman:$MANPATH"
+
