e9eb2931edd70010f8cee1b23bdbc257601fc7ca
Brendan Boerner <bboerner.foss@gmail.com>
2014-11-09 00:36:37 -0600

    Find my 'global' node modules

--- nodejs/path.bash
+++ nodejs/path.bash
@@ -4,3 +4,7 @@ if [[ -d $NODE_ROOT/bin ]]; then
     [[ -d $NODE_ROOT/man ]] && export MANPATH="$NODE_ROOT/man:$MANPATH"
     [[ -d $NODE_ROOT/lib ]] && export LD_LIBRARY_PATH="$NODE_ROOT/lib:$LD_LIBRARY_PATH"
 fi
+
+# modules
+NODE_ROOT="$HOME/usr/local/node_modules"
+[[ -d $NODE_ROOT/.bin ]] && export PATH="$NODE_ROOT/.bin:$PATH"
