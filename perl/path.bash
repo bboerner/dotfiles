604e67520755b60bd0e700895e6be49b539e9d4d
Brendan Boerner <bboerner.foss@gmail.com>
2014-11-09 15:53:18 -0600

    perl/

--- perl/path.bash
+++ perl/path.bash
@@ -1,2 +1,6 @@
-CPAN_ROOT="$HOME/perl5"
-[[ -d $CPAN_ROOT/bin ]] && export PATH=$CPAN_ROOT/bin:$PATH
+PERL_ROOT="$HOME/perl5"
+[[ -d $PERL_ROOT/bin ]] && export PATH=$PERL_ROOT/bin:$PATH
+
+PERL_MB_OPT="--install_base \"$PERL_ROOT\""; export PERL_MB_OPT;
+PERL_MM_OPT="INSTALL_BASE=$PERL_ROOT"; export PERL_MM_OPT;
+PERL5LIB="$PERL_ROOT/lib/perl5"; export PERL5LIB;
