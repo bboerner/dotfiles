dc2d2eb8888e5fd987738b39b1b50b97525bec6e
Brendan Boerner <bboerner.foss@gmail.com>
2014-12-05 22:52:52 -0600

    Check for existence of dbus-uuidgen before trying to use it

--- dbus/init.bash
+++ dbus/init.bash
@@ -1,10 +1,11 @@
 # Using git-credential-gnome-keyring over ssh.
 # From https://marklodato.github.io/2013/10/25/github-two-factor-and-gnome-keyring.html
 
-if [[ -z $DBUS_SESSION_BUS_ADDRESS ]]; then
-    if [[ -f ~/.dbus/session-bus/$(dbus-uuidgen --get)-0 ]]; then
-        source ~/.dbus/session-bus/$(dbus-uuidgen --get)-0
-        export DBUS_SESSION_BUS_ADDRESS
+if $(which dbus-uuidgen 2>&1 > /dev/null) ; then
+    if [[ -z $DBUS_SESSION_BUS_ADDRESS ]]; then
+        if [[ -f ~/.dbus/session-bus/$(dbus-uuidgen --get)-0 ]]; then
+            source ~/.dbus/session-bus/$(dbus-uuidgen --get)-0
+            export DBUS_SESSION_BUS_ADDRESS
+        fi
     fi
 fi
-
