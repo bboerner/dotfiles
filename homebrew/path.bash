d216491c3666858b92d907db477fb598729fec49
Brendan Boerner <bboerner.foss@gmail.com>
2014-12-05 23:04:15 -0600

    Setup path for homebrew, if found

---
+++ homebrew/path.bash
@@ -0,0 +1,11 @@
+BREW_ROOT="$HOME/.homebrew"
+BREW_OPT_LIBEXEC="$BREW_ROOT/Cellar/coreutils/8.22/libexec"
+
+# Use the 'g' prefixed commands
+[[ -d $BREW_ROOT/bin ]] && export PATH="$BREW_ROOT/bin:$PATH"
+[[ -d $BREW_ROOT/lib ]] && export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$BREW_ROOT/lib"
+
+# w/o the 'g' prefix
+#[[ -d $BREW_OPT_LIBEXEC/gnubin ]] && export PATH="$BREW_OPT_LIBEXEC/gnubin:$PATH"
+#[[ -d $BREW_OPT_LIBEXEC/gnuman ]] && export MANPATH="$BREW_OPT_LIBEXEC/gnuman:$MANPATH"
+
