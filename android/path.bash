ca78a4301ebe37ec666afe61b4ce7f5d4e059099
Brendan Boerner <bboerner.foss@gmail.com>
2015-12-01 07:35:48 -0600

    Support Android studio

--- android/path.bash
+++ android/path.bash
@@ -1,8 +1,8 @@
 function add_android() {
     echo "Adding Android"
 
-    ANDROIDSDK="/home/bboerner/usr/local/android-sdk-linux"
-    ANDROIDNDK="/home/bboerner/usr/local/android-ndk-r9b"
+    ANDROIDSDK="$HOME/usr/local/android-sdk-linux"
+    ANDROIDNDK="$HOME/usr/local/android-ndk-r9b"
 
     if [[ -d $ANDROIDSDK && -d $ANDROIDNDK ]] ; then
         export ANDROIDAPI=19
@@ -15,6 +15,27 @@ function add_android() {
 
     else
         echo "error: $ANDROIDSDK or $ANDROIDNDK not found."
+
+    fi
+}
+
+function add_studio() {
+    echo "Adding Android Studio"
+
+    STUDIO_ROOT="$HOME/usr/local/android-studio"
+    STUDIO="$STUDIO_ROOT/bin"
+
+    if [[ -d $STUDIO ]] ; then
+        export ANDROIDAPI=19
+        export ANDROIDNDKVER=r9b
+        export PATH=$STUDIO:$PATH
+
+        #export STUDIO
+
+        PS1="[android studio] ${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
+
+    else
+        echo "error: $STUDIO not found."
 
     fi
 }
