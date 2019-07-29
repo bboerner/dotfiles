9b059c3454fa8a3aa8f05b1a1722d49d41c3ede7
Brendan Boerner <bboerner.foss@gmail.com>
2019-07-29 05:15:20 -0500

    Remove cruft

--- nodejs/path.bash
+++ nodejs/path.bash
@@ -1,18 +1,3 @@
-if false; then
-#NODE_ROOT="$HOME/usr/local/node-v0.10.32"
-#NODE_ROOT="$HOME/usr/local/node-v0.10.40-linux-x64"
-#NODE_ROOT="$HOME/usr/local/node-v4.0.0-linux-x64"
-#NODE_ROOT="$HOME/usr/local/node-v0.12.7-linux-x64"
-#NODE_ROOT="$HOME/usr/local/node-v0.12.7"
-NODE_ROOT="$HOME/usr/local/node-v8.11.1-linux-x64/"
-
-if [[ -d $NODE_ROOT/bin ]]; then
-    export PATH="$NODE_ROOT/bin:$PATH"
-    [[ -d $NODE_ROOT/man ]] && export MANPATH="$NODE_ROOT/man:$MANPATH"
-    [[ -d $NODE_ROOT/lib ]] && export LD_LIBRARY_PATH="$NODE_ROOT/lib:$LD_LIBRARY_PATH"
-fi
-fi
-
 # 3/25/19: nvm
 
 # modules
