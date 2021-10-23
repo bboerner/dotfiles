3087526e0efd8e13b145848878680945f31c20f5
Brendan Boerner <bboerner.foss@gmail.com>
2021-10-23 10:40:32 -0400

    Test brew first; disable it

--- linuxbrew/path.bash
+++ linuxbrew/path.bash
@@ -1,5 +1,8 @@
 # 5/14/19
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+# 10/18/20: Disable linuxbrew. Lot of conflicts on ubuntu 18.04. Maybe
+# turn into a function.
+#[[ -f /home/linuxbrew/.linuxbrew/bin/brew ]] && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
 # w/o the 'g' prefix
 #PATH="$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin:$PATH"
 
