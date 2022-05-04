c07aba77fb7a98bdcaafbceb2bdf9dacfc47cc8d
Brendan Boerner <bboerner.foss@gmail.com>
2022-05-03 19:40:49 -0500

    a. ulimit -c 10737418240 should be sufficient.

--- 00system/env.bash
+++ 00system/env.bash
@@ -11,7 +11,9 @@ if [[ "${kernel_name%*_NT-5*}" = "CYGWIN" || "${kernel_name%*_NT-6*}" = "CYGWIN"
 fi
 
 if [[ $cygwin -eq 0 ]]; then
-    ulimit -c 1000000000;
+    #ulimit -c 1000000000;
+    # 10 Gb
+    ulimit -c 10737418240;
 else
     ulimit -c 1000000;
 fi
